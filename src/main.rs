dd65270770feb787014b7db5239d446275444e94
Paul Miller <paul@pauljmiller.com>
2021-03-13 11:01:35 -0500

    prompt user to specify coldcard path up front

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,40 @@
+use address_factory::wizard_steps::*;
 use anyhow::Result;
-use signed_address_generator::wizard_steps::*;
+use std::path::PathBuf;
+
+use clap::Clap;
+
+#[derive(Clap)]
+struct Opts {
+    coldcard_json: Option<PathBuf>,
+}
 
 fn main() -> Result<()> {
-    let mode = select_mode()?;
+    let opts: Opts = Opts::parse();
 
-    let mut factory = match mode {
-        Mode::New(submode) => match submode {
-            SubMode::Coldcard => {
-                let network = select_network()?;
-                let descriptor = new_coldcard(network)?;
-                new_factory(descriptor, network, 0, 1000)
-            }
-            SubMode::Generic => {
-                let network = select_network()?;
-                let descriptor = new_generic(network)?;
-                check_first_address(descriptor.clone(), network)?;
-                new_factory(descriptor, network, 0, 1000)
-            }
-        },
-        Mode::Continue => load_and_edit_factory(),
-    }?;
+    if let Some(path) = opts.coldcard_json {
+        let (descriptor, network) = new_coldcard_from_file(&path)?;
+        let mut factory = new_factory(descriptor, network, 0, 1000)?;
+        execute(&mut factory)
+    } else {
+        let mode = select_mode()?;
 
-    execute(&mut factory)
+        let mut factory = match mode {
+            Mode::New(submode) => match submode {
+                SubMode::Coldcard => {
+                    new_coldcard_instruction();
+                    return Ok(());
+                }
+                SubMode::Generic => {
+                    let network = select_network()?;
+                    let descriptor = new_generic(network)?;
+                    check_first_address(descriptor.clone(), network)?;
+                    new_factory(descriptor, network, 0, 1000)
+                }
+            },
+            Mode::Continue => load_and_edit_factory(),
+        }?;
+
+        execute(&mut factory)
+    }
 }
